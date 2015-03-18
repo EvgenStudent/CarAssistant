becaa50b3c80d04b73f3586b6ec08030965185e4
EvgenStudent <ereviakin@gmail.com>
2015-03-18 15:31:00 +0300

    v. 0.0.8 Start realization FuelStationDataReader

---
+++ CarAssistant/CarAssistant/public/javascripts/custom/FuelStationDataReader.js
@@ -0,0 +1,16 @@
+﻿var request = require('request');
+
+var url = "http://nominatim.openstreetmap.org/search?q=belarus[fuel]&format=xml&polygon=0&addressdetails=1&limit=1000";
+
+
+var getFuelStations = ReadByUrl(url);
+
+function ReadByUrl(url) {
+    request(url, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body);
+        }
+    });
+}
+
+module.exports = getFuelStations;
--- CarAssistant/CarAssistant/routes/routes.js
+++ CarAssistant/CarAssistant/routes/routes.js
@@ -1,6 +1,14 @@
 ﻿var express = require('express');
 var router = express.Router();
 
+//==============
+
+var fuelStations = require('../public/javascripts/custom/fuelStationDataReader');
+var test = fuelStationData.getFuelStations();
+var ttt = 1 + 1;
+
+//==============
+
 var indexData = { x: 53.6733327, y: 23.826727 };
 var fuelStationData = { from: { x: 53.642946, y: 23.842051 }, to: { x: 53.676540, y: 23.825031 } };
 
