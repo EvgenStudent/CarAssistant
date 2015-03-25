4bd7c9bfdb24865d52d4469769d076a9794074b4
marianezhyvinskaya <mari4ka.13@mail.ru>
2015-03-26 00:37:04 +0300

    v. 0.0.9 Project merge from VS to WebStorm.

---
+++ CarAssistant/public/javascripts/custom/FuelStationDataReader.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Masha on 25.03.2015.
+ */
+var request = require('request');
+
+var url = "http://nominatim.openstreetmap.org/search?q=belarus[fuel]&format=xml&polygon=0&addressdetails=1&limit=1000";
+
+
+var getFuelStations = ReadByUrl(url);
+
+function ReadByUrl(url) {
+    request(url, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body);
+        }
+    });
+}
+
+module.exports = getFuelStations;
