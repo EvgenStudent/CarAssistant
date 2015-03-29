7d26a93d188b75378319e0187793ef66766c8c90
EvgenStudent <ereviakin@gmail.com>
2015-03-29 15:26:48 +0300

    v. 0.1.1 Add angularjs and fuelStationController

---
+++ CarAssistent.Client/public/controllers/fuelStationController.js
@@ -0,0 +1,8 @@
+var app = angular.module('main', ['$http', '$scope', '$localStorage']);
+
+app.controller('fuelStationController', function($http, $scope, $localStorage) {
+            $scope.getAllStations = function (object) {
+
+            };
+        }
+    );
